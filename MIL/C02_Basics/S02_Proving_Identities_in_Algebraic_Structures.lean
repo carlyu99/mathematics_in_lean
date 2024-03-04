d60f9ace5f33687271a33dc7a3d2bc9977930f9f
Carl Yu <carlyu99@163.com>
2024-03-04 13:55:36 -0500

    solve 2.1 and 2.2

--- MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
+++ MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
@@ -53,13 +53,13 @@ theorem neg_add_cancel_left (a b : R) : -a + (a + b) = b := by
 
 -- Prove these:
 theorem add_neg_cancel_right (a b : R) : a + b + -b = a := by
-  sorry
+  rw [add_assoc, add_right_neg, add_zero]
 
 theorem add_left_cancel {a b c : R} (h : a + b = a + c) : b = c := by
-  sorry
+  rw [← neg_add_cancel_left a b, h, neg_add_cancel_left a c]
 
 theorem add_right_cancel {a b c : R} (h : a + b = c + b) : a = c := by
-  sorry
+  rw [← add_neg_cancel_right a b, h, add_neg_cancel_right c b]
 
 theorem mul_zero (a : R) : a * 0 = 0 := by
   have h : a * 0 + a * 0 = a * 0 + 0 := by
@@ -67,20 +67,27 @@ theorem mul_zero (a : R) : a * 0 = 0 := by
   rw [add_left_cancel h]
 
 theorem zero_mul (a : R) : 0 * a = 0 := by
-  sorry
+  apply add_left_cancel (a := 0 * a)
+  rw [← add_mul, add_zero, add_zero]
 
 theorem neg_eq_of_add_eq_zero {a b : R} (h : a + b = 0) : -a = b := by
-  sorry
+  rw [← add_zero (-a), ← h, ← add_assoc, add_left_neg, add_comm, add_zero]
 
 theorem eq_neg_of_add_eq_zero {a b : R} (h : a + b = 0) : a = -b := by
-  sorry
+  have h1 : a = a + b + -b := by
+    rw [add_assoc, add_right_neg, add_zero]
+  rw [h, add_comm, add_zero] at h1
+  exact h1
 
 theorem neg_zero : (-0 : R) = 0 := by
   apply neg_eq_of_add_eq_zero
   rw [add_zero]
 
-theorem neg_neg (a : R) : - -a = a := by
-  sorry
+theorem neg_neg (a : R) : - -a = a := by  -- This proof doesn't use neg_zero.
+  have h : - -a + -a = 0 := by
+    rw [add_comm, add_right_neg]
+  rw [← add_right_neg a] at h
+  exact add_right_cancel h
 
 end MyRing
 
@@ -103,13 +110,13 @@ namespace MyRing
 variable {R : Type*} [Ring R]
 
 theorem self_sub (a : R) : a - a = 0 := by
-  sorry
+  rw [sub_eq_add_neg, add_right_neg]
 
 theorem one_add_one_eq_two : 1 + 1 = (2 : R) := by
   norm_num
 
 theorem two_mul (a : R) : 2 * a = a + a := by
-  sorry
+  rw [← one_add_one_eq_two, add_mul, one_mul]
 
 end MyRing
 
@@ -143,4 +150,3 @@ theorem mul_inv_rev (a b : G) : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by
 end MyGroup
 
 end
-
