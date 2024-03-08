fa82842b57d483e07c3d873cbd84c2fc41379e29
Carl Yu <carlyu99@163.com>
2024-03-08 17:35:39 -0500

    Update S04_More_on_Order_and_Divisibility.lean

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,31 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply max_le
+    apply le_max_right
+    apply le_max_left
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply le_min
+    simp [min_le_left]
+    simp [min_le_left]
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  repeat simp [min_le_left]
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  apply aux
+  have h := aux (a + c) (b + c) (-c)
+  rw [add_neg_cancel_right, add_neg_cancel_right] at h
+  linarith
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  nth_rw 1 [← sub_add_cancel a b]
+  linarith [abs_add (a - b) b]
 end
 
 section
@@ -66,7 +80,12 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  repeat apply dvd_add
+  · apply dvd_mul_of_dvd_right
+    apply dvd_mul_right
+  · apply dvd_mul_left
+  · apply dvd_mul_of_dvd_left
+    simp [h]
 end
 
 section
@@ -78,7 +97,9 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply dvd_antisymm
+  repeat
+    apply Nat.dvd_gcd
+    apply Nat.gcd_dvd_right
+    apply Nat.gcd_dvd_left
 end
-
-
